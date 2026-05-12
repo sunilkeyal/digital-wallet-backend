b8920e25a67552291abccb02444c59806938c111
Sunil Keyal <sunilkeyal@hotmail.com>
2026-05-12 07:45:53 -0400

    fix deploy take 2

--- src/test/java/com/digitalwallet/DigitalWalletApplicationTests.java
+++ src/test/java/com/digitalwallet/DigitalWalletApplicationTests.java
@@ -1,9 +1,7 @@
 package com.digitalwallet;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DigitalWalletApplicationTests {
 
     @Test
